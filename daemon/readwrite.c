cbodley/ms-nfs41-client

commit 1f7e560a9a93d2413518b6c93777ff7706e0031b

pnfs: fix for short write on ds error
Signed-off-by: Casey Bodley <cbodley@citi.umich.edu>

daemon/readwrite.c
@@ -271,16 +271,13 @@ static int handle_write(nfs41_upcall *upcall)
 {
     readwrite_upcall_args *args = &upcall->args.rw;
     stateid_arg stateid;
-    ULONG pnfs_bytes_written = 0;
+    uint32_t pnfs_bytes_written = 0;
     int status;
 
     nfs41_open_stateid_arg(upcall->state_ref, &stateid);
 
 #ifdef PNFS_ENABLE_WRITE
     status = write_to_pnfs(upcall, &stateid);
-    if (status == NO_ERROR)
-        goto out;
-
     if (args->out_len) {
         pnfs_bytes_written = args->out_len;
         args->out_len = 0;
@@ -288,6 +285,9 @@ static int handle_write(nfs41_upcall *upcall)
         args->offset += pnfs_bytes_written;
         args->buffer += pnfs_bytes_written;
         args->len -= pnfs_bytes_written;
+
+        if (args->len == 0)
+            goto out;
     }
 #endif
 

commit fb7c379f06326a271f238b665e14aac545997261

pnfs: fix for write length returned
fixes an issue introduced by commit 1f7e560a9a93d2413518b6c93777ff7706e0031b, "pnfs: fix for short write on ds error"

when a write is satisfied completely by pnfs, the upcall was returning out_len=0.  the only time i noticed this was against python server, where cp in cygwin would succeed but print 'No space left on device'

Signed-off-by: Casey Bodley <cbodley@citi.umich.edu>

daemon/readwrite.c
@@ -296,9 +296,8 @@ static int handle_write(nfs41_upcall *upcall)
 #endif
 
     status = write_to_mds(upcall, &stateid);
-
-    args->out_len += pnfs_bytes_written;
 out:
+    args->out_len += pnfs_bytes_written;
     return status;
 }
 